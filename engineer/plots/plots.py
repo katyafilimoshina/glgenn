a60e957b60b6232b4b09ee2892248a4eff86c465
katyafilimoshina <filimoshinaek@gmail.com>
2024-05-28 09:48:27 +0300

    Add function for models comparison

--- engineer/plots/plots.py
+++ engineer/plots/plots.py
@@ -75,3 +75,33 @@ def plot_test_losses(train_losses, test_losses, num_to_show):
     plt.legend()
     plt.tight_layout()
     plt.show()
+
+
+def plot_losses_compare(train_losses_list, val_losses_list, model_labels):
+    """
+    Plots the training and validation losses for multiple models side by side.
+
+    :param train_losses_list: List of lists, where each sublist contains training losses for a model.
+    :param val_losses_list: List of lists, where each sublist contains validation losses for a model.
+    :param model_labels: List of strings, where each string is a label for a model.
+    """
+    def plot_loss_c(ax, steps, losses_list, title, model_labels, test=False):
+        for losses, label in zip(losses_list, model_labels):
+            ax.plot(steps, losses, label=label)
+        if test:
+          ax.set_xlabel(f'Step (test was on each {int(val_check_interval)} step of training)')
+        else:
+          ax.set_xlabel('Step')
+        ax.set_ylabel('Loss')
+        ax.set_title(title)
+        ax.legend()
+        ax.grid(True)
+
+    val_check_interval = len(train_losses_list[0]) / len(val_losses_list[0])
+    steps = range(len(train_losses_list[0]))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
+    plot_loss_c(ax1, steps, train_losses_list, 'Train Loss', model_labels)
+    plot_loss_c(ax2, steps, val_losses_list, 'Test Loss', model_labels, test=True)
+    plt.tight_layout()
+    plt.show()
