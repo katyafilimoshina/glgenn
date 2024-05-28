ed97d8861e639f53f0ac0955b9e3680686a987ec
katyafilimoshina <filimoshinaek@gmail.com>
2024-05-28 09:05:11 +0300

    Add function for train-test plot

--- engineer/plots/plots.py
+++ engineer/plots/plots.py
@@ -36,6 +36,32 @@ def plot_losses(train_losses, val_losses, test_losses):
     plt.show()
 
 
+def plot_traintest_losses(train_losses, val_losses):
+    """
+    Plots train_losses and val_losses
+    """
+    val_check_interval = len(train_losses) / len(val_losses)
+    
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(train_losses, label='Train Loss')
+    plt.xlabel('Step')
+    plt.ylabel('Loss')
+    plt.title('Training Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(np.arange(0, len(train_losses), val_check_interval), val_losses, label='Test Loss', color='green')
+    plt.xlabel(f'Step (test was on each {int(val_check_interval)} step of training)')
+    plt.ylabel('Loss')
+    plt.title('Test Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
 def plot_test_losses(train_losses, test_losses, num_to_show):
     """
     Plots the last num_to_show items of test_losses
